1cf08dbc196f94a289cd04e4d2b01a1a5a62de93
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2015-12-25 23:44:49 -0800

    day 1 part b

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,17 +5,30 @@ fn main() {
     let mut file = File::open("input.txt").unwrap();
     let mut input = String::new();
     file.read_to_string(&mut input).unwrap();
-    println!("Part A: floor {}", process_a(input));
+    println!("Part A: floor {}", process_a(input.clone()));
+    println!("Part B: instruction {}", process_b(input.clone()));
 }
 
 fn process_a(input: String) -> isize {
-    input.chars().fold(0, |acc, item| {
-        match item {
-            '(' => acc + 1,
-            ')' => acc - 1,
-            _ => acc
+    input.chars().fold(0, |floor, instruction| {
+        match instruction {
+            '(' => floor + 1,
+            ')' => floor - 1,
+            _ => floor
         }
     })
+}
+
+fn process_b(input: String) -> isize {
+    let answer = input.chars().scan(0, |floor, instruction| {
+        *floor = match instruction {
+            '(' => *floor + 1,
+            ')' => *floor - 1,
+            _ => *floor
+        };
+        Some(*floor)
+    }).position(|floor| floor == -1).unwrap_or(0);
+    answer as isize + 1
 }
 
 #[test]
@@ -45,4 +58,13 @@ fn example_a_4() {
 fn example_a_5() {
     assert_eq!(process_a(")))".to_owned()), -3);
     assert_eq!(process_a(")())())".to_owned()), -3);
+}
+
+#[test]
+fn example_b_1() {
+    assert_eq!(process_b(")".to_owned()), 1);
+}
+#[test]
+fn example_b_2() {
+    assert_eq!(process_b("()())".to_owned()), 5);
 }
