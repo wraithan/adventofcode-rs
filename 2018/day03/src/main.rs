0bd1e4b59373d0a0987b2810a76a3ec67c33fe39
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2018-12-12 08:19:21 -0800

    2018-D03-P2

--- 2018/day03/src/main.rs
+++ 2018/day03/src/main.rs
@@ -35,12 +35,39 @@ fn solve_puzzle_part_1(input: &str) -> Result<u32, String> {
 }
 
 fn solve_puzzle_part_2(input: &str) -> Result<u32, String> {
-    let list = input.lines().filter(|label| !label.is_empty());
-    Ok(list.count() as u32)
+    let list = input
+        .lines()
+        .filter(|label| !label.is_empty())
+        .map(parse_line);
+    let mut fabric = HashMap::new();
+    for order in list.clone() {
+        for x in order.x..(order.x + order.width) {
+            for y in order.y..(order.y + order.height) {
+                let coords = format!("{},{}", x, y);
+                let count = fabric.entry(coords).or_insert(0);
+                *count += 1;
+            }
+        }
+    }
+
+    'order_loop: for order in list {
+        for x in order.x..(order.x + order.width) {
+            for y in order.y..(order.y + order.height) {
+                let coords = format!("{},{}", x, y);
+                let value = fabric.get(&coords).unwrap();
+                if value > &1 {
+                    continue 'order_loop;
+                }
+            }
+        }
+        return Ok(order.number);
+    }
+    Err("not found".into())
 }
 
 #[derive(Debug)]
 struct Order {
+    number: u32,
     x: u32,
     y: u32,
     width: u32,
@@ -49,19 +76,30 @@ struct Order {
 
 fn parse_line(line: &str) -> Order {
     let mut parts = line.split(' ');
-    let _number = parts.next().unwrap();
+    let number = parts
+        .next()
+        .unwrap()
+        .trim_start_matches('#')
+        .parse()
+        .unwrap();
     let _at = parts.next().unwrap();
     let pos = parts.next().unwrap();
     let size = parts.next().unwrap();
 
     let mut pos_parts = pos.split(',');
     let x = pos_parts.next().unwrap().parse().unwrap();
-    let y = pos_parts.next().unwrap().trim_end_matches(':').parse().unwrap();
+    let y = pos_parts
+        .next()
+        .unwrap()
+        .trim_end_matches(':')
+        .parse()
+        .unwrap();
 
     let mut size_parts = size.split('x');
     let width = size_parts.next().unwrap().parse().unwrap();
     let height = size_parts.next().unwrap().parse().unwrap();
     Order {
+        number,
         x,
         y,
         width,
@@ -86,8 +124,8 @@ mod test_part_2 {
     use super::solve_puzzle_part_2;
     #[test]
     fn example_01() {
-        let input = "";
-        let expected = 0;
+        let input = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2\n";
+        let expected = 3;
         let result = solve_puzzle_part_2(input).unwrap();
         assert_eq!(result, expected)
     }
